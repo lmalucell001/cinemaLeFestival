4a4ad226bd634aaa20a61ad2266ef010a6902f5c
Wormdragon7 <dinogopal@yahoo.fr>
2020-10-29 22:09:14 +0000

    last modif before time's up

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ window.addEventListener('scroll', function () {
 
     const mapY = map.getBoundingClientRect().y;
     console.log(mapY)
-    if (mapY < 2100 && mapY > -580) {
+    if (mapY < 2100 && mapY > 200) {
         contpuces.classList.add('is-black');
         anchors.forEach(function (title, index,) {
             const titleY = title.getBoundingClientRect().y;
